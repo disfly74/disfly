239112b734ff1b27b5503b48efe2cb48702b7994
Maxime Karababa <disfly.site@gmail.com>
2025-09-12 23:14:44 +0200

    Passage à la V1.9 - Correctifs et Amélioration du design global

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
     var form = document.getElementById("contactForm");
-    var status = document.getElementById("formStatus");
-    
-    async function handleSubmit(event) {
-        event.preventDefault();
-        var data = new FormData(event.target);
-        fetch(event.target.action, {
-            method: form.method,
-            body: data,
-            headers: {
-                "Accept": "application/json"
-            }
-        }).then(response => {
-            if (response.ok) {
-                status.textContent = "Message envoyé avec succès !";
-                status.style.color = "green";
-                form.reset();
-            } else {
-                response.json().then(data => {
-                    if (Object.hasOwn(data, "errors")) {
-                        status.textContent = data["errors"].map(error => error["message"]).join(", ");
-                        status.style.color = "red";
-                    } else {
-                        status.textContent = "Oops! Il y a eu un problème lors de l'envoi.";
-                        status.style.color = "red";
-                    }
-                })
-            }
-        }).catch(error => {
-            status.textContent = "Oops! Il y a eu un problème lors de l'envoi.";
-            status.style.color = "red";
-        });
+    if (form) {
+        var status = document.getElementById("formStatus");
+        async function handleSubmit(event) {
+            event.preventDefault();
+            var data = new FormData(event.target);
+            fetch(event.target.action, {
+                method: form.method,
+                body: data,
+                headers: {
+                    "Accept": "application/json"
+                }
+            }).then(response => {
+                if (response.ok) {
+                    status.textContent = "Message envoyé avec succès !";
+                    status.style.color = "green";
+                    form.reset();
+                } else {
+                    response.json().then(data => {
+                        if (Object.hasOwn(data, "errors")) {
+                            status.textContent = data["errors"].map(error => error["message"]).join(", ");
+                            status.style.color = "red";
+                        } else {
+                            status.textContent = "Oops! Il y a eu un problème lors de l'envoi.";
+                            status.style.color = "red";
+                        }
+                    })
+                }
+            }).catch(error => {
+                status.textContent = "Oops! Il y a eu un problème lors de l'envoi.";
+                status.style.color = "red";
+            });
+        }
+        form.addEventListener("submit", handleSubmit);
     }
-    form.addEventListener("submit", handleSubmit);
 
-    // Fonction d'animation au scroll
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {

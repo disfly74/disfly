e35dcb2b9ae0e4bf4c1328182628a109e67978f0
Maxime Karababa <disfly.site@gmail.com>
2025-09-12 23:04:01 +0200

    Passage à la V1.8 - Correctifs et Amélioration du design global

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-// Fonction pour gérer l'apparition des éléments
-function handleIntersection(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-            observer.unobserve(entry.target);
-        }
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById("contactForm");
+    var status = document.getElementById("formStatus");
+    
+    async function handleSubmit(event) {
+        event.preventDefault();
+        var data = new FormData(event.target);
+        fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                "Accept": "application/json"
+            }
+        }).then(response => {
+            if (response.ok) {
+                status.textContent = "Message envoyé avec succès !";
+                status.style.color = "green";
+                form.reset();
+            } else {
+                response.json().then(data => {
+                    if (Object.hasOwn(data, "errors")) {
+                        status.textContent = data["errors"].map(error => error["message"]).join(", ");
+                        status.style.color = "red";
+                    } else {
+                        status.textContent = "Oops! Il y a eu un problème lors de l'envoi.";
+                        status.style.color = "red";
+                    }
+                })
+            }
+        }).catch(error => {
+            status.textContent = "Oops! Il y a eu un problème lors de l'envoi.";
+            status.style.color = "red";
+        });
+    }
+    form.addEventListener("submit", handleSubmit);
+
+    // Fonction d'animation au scroll
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
     });
-}
 
-// Options de l'observateur
-const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.2
-};
-
-// Créer un nouvel observateur
-const observer = new IntersectionObserver(handleIntersection, options);
-
-// Cibler tous les éléments avec la classe 'hidden'
-document.querySelectorAll('.hidden').forEach(element => {
-    observer.observe(element);
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
 });
